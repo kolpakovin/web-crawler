3addb18ea8a6da56d0f66f6097432cc870f31cdc
kolpakovin <55667539+kolpakovin@users.noreply.github.com>
2020-06-14 13:57:13 +0300

    database class was created

---
+++ database.py
@@ -0,0 +1,25 @@
+import pymongo
+
+class Database:
+    URI = 'mongodb://127.0.0.1:27017'
+    Database = None
+
+    @staticmethod
+    def initialize():
+        pass
+
+    @staticmethod
+    def insert(collection, data):
+        pass
+
+    @staticmethod
+    def find():
+        pass
+
+    @staticmethod
+    def update():
+        pass
+
+    @staticmethod
+    def count_documents():
+        pass
