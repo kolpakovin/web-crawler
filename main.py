50672048fed8a45e1a6f38528614a425e9bb2d91
kolpakovin <55667539+kolpakovin@users.noreply.github.com>
2020-06-14 14:41:03 +0300

    our tool can get firmware downloads links with help of few simple methods

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 class Crawler:
 
     def __init__(self,url):
-        self.url = url
+        self.website_url = url
         self.firmware_downloads = ''
         self.pages = []
         self.firmware_files_links = []
 
     def get_page_content(self, url):
-        pass
+        page = requests.get(url)
+        return BeautifulSoup(page.content, 'html.parser')
     
     def get_element_from_content(self, content, element, class_or_id=None):
-        pass
+        if class_or_id is None:
+            return content.find(element)
+        return content.find(element, class_or_id)
     
     def get_elements_from_content(self, content, element, class_or_id=None):
-        pass
+        if class_or_id is None:
+            return content.find_all(element)
+        return content.find_all(element, class_=class_or_id)
     
-    def get_firmware_download_link(self):
-        pass
-    
+    def get_firmware_downloads_link(self):
+        soup = self.get_page_content(self.website_url)
+        the_link = self.get_element_from_content(soup, 'tr').find('a')['href']
+        self.firmware_downloads = self.website_url + the_link
+        return self.firmware_downloads
+
+
     def get_next_page_link(self, url):
         pass
     
@@ -36,7 +49,7 @@ class Crawler:
     def get_metadata_from_link(self, url):
         pass
 
-    def add_to_db(self)
+    def add_to_db(self):
         pass
     
     def get_metadata_and_add_to_db(self):
